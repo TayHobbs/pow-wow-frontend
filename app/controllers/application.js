0425863c5a4a92aba7627de1dc53952b609c3acd
Taylor Hobbs <taylor@feemon.com>
2015-03-18 17:05:44 -0500

    Refactor: remove console log

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,6 +4,5 @@ import ENV from 'pow-wow-frontend/config/environment';
 export default Ember.Controller.extend({
   updateToken: function() {
     ENV.APP.accessToken = this.get('localStorageProxy.accessToken');
-    console.log(ENV.APP.accessToken);
   }.observes("localStorageProxy.accessToken").on("init")
 });
