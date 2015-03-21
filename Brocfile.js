10a9196afbd0335891a2de3541de72e1b22a240d
Taylor Hobbs <taylor@feemon.com>
2015-03-21 06:55:55 -0500

    Bug Fix: wait to export app until I have imported my dependencies

--- Brocfile.js
+++ Brocfile.js
@@ -4,9 +4,10 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
-module.exports = app.toTree();
 
 if (app.env === 'test' || app.env === 'development') {
   app.import('bower_components/fauxjax/dist/fauxjax.min.js');
 }
 app.import('bower_components/lodash/lodash.min.js');
+
+module.exports = app.toTree();
