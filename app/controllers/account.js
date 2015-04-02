ee840e5c27fe476c18488ab5f701ae47ade075b4
Taylor Hobbs <taylor@feemon.com>
2015-04-01 20:27:29 -0500

    WIP

--- app/controllers/account.js
+++ app/controllers/account.js
@@ -3,6 +3,15 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   flashMessage: null,
 
+  timeOutFlashMessage: function() {
+    var controller = this;
+    if (this.flashMessage) {
+      Ember.run.later(() => {
+        this.send('dismissFlashMessage');
+      }, 3000);
+    }
+  }.observes('flashMessage').on('init'),
+
   actions: {
     dismissFlashMessage: function() {
       this.set('flashMessage', null);
