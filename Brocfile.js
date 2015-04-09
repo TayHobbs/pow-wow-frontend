acfd3e23049f3107578d53a7b5aaf6b251daa01f
Taylor Hobbs <taylor@feemon.com>
2015-04-08 19:53:55 -0500

    Feature: Fix mobile navbar

--- Brocfile.js
+++ Brocfile.js
@@ -10,6 +10,9 @@ var app = new EmberApp({
 if (app.env === 'test' || app.env === 'development') {
   app.import('bower_components/fauxjax/dist/fauxjax.min.js');
 }
+
 app.import('bower_components/lodash/lodash.min.js');
+app.import('bower_components/foundation/js/foundation.min.js');
+app.import('bower_components/foundation/js/foundation/foundation.topbar.js');
 
 module.exports = app.toTree();
---
+++ app/views/application.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.View.extend({
+  didInsertElement: function() {
+    this.$().foundation('topbar');
+  },
+
+  willDestroyElement: function() {
+    this.$().foundation('topbar', 'off');
+  }
+});
