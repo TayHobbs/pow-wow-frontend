5d753ec382d0a69f0430afe14813f22d4d3593a7
Taylor Hobbs <taylor@feemon.com>
2015-04-20 22:09:44 -0500

    Refactor: Delete useless namespace in adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,6 @@ import ENV from 'pow-wow-frontend/config/environment';
 import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.extend({
-  namespace: '',
   host: ENV.apiDomain,
   headers: Ember.computed(function() {
     return { 'Authorization': this.get('localStorageProxy.accessToken') }
