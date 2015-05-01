f4a7dd43076f10da310a624416fcd4398512d491
Taylor Hobbs <taylor@feemon.com>
2015-05-01 07:49:09 -0500

    Style: Fix JSHint errors

--- app/controllers/forgotten-password.js
+++ app/controllers/forgotten-password.js
@@ -4,14 +4,13 @@ import ENV from 'pow-wow-frontend/config/environment';
 export default Ember.Controller.extend({
   actions: {
     sendForgottenPasswordEmail() {
-      let email = this.get('email');
       Ember.$.ajax({
         method: 'GET',
         url: `${ENV.apiDomain}/users/${this.get('email')}/forgot`,
-        success: (results) => {
-          Ember.run.later(() => { this.set('emailSent', true)} );
+        success: () => {
+          Ember.run.later(() => { this.set('emailSent', true); });
         },
-        error: (jqXHR, status, error) => {
+        error: (jqXHR) => {
           Ember.run.later(() => {
             Ember.get(this, 'flashMessages').add({
               message: 'We have no record of that email, please enter another.', sticky: ENV.stickyFlash
