1e497d7320e5723429e1210587df11b5e178cb9a
Taylor Hobbs <taylor@feemon.com>
2015-06-18 08:50:46 -0500

    Dependency: Update Ember-Data

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,9 @@
 import Ember from 'ember';
-import DS from 'ember-data';
+import ActiveModelAdapter from 'active-model-adapter';
 
 import ENV from 'pow-wow-frontend/config/environment';
 
-export default DS.ActiveModelAdapter.extend({
+export default ActiveModelAdapter.extend({
   host: ENV.apiDomain,
   headers: Ember.computed(function() {
     return { 'Authorization': this.get('localStorageProxy.accessToken') };
